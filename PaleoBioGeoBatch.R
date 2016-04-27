1db01901d7a301b670f785675fe7cac89ba80868
Ryan Roney <Ryan Roney>
2016-04-27 16:17:22 -0400

    thinking through the process

--- PaleoBioGeoBatch.R
+++ PaleoBioGeoBatch.R
@@ -1,11 +1,11 @@
 
 source("PaleoBioGeoFunctions")
 tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
-Localities <- read.csv("___PATH_to_File_with_locality_coordinates_by_species____")
-# file with localities of of species, including GPS coordinates of locality or a centroid of the basin
-# this should include all instances of a species, and assusmes identifications are confirmed
+Localities <- read.csv("___PATH_to_File_with_locality_coordinates____")
+# file with localities of of species, including GPS coordinates of locality or a centroid of the basin. localities have unique identifiers matched with those used in 'species.localities.stratigiraphy' file. These do not repeat in this file.
 
-Stratigraphy <- read.csv("____PATH_to_file_with_ages_and_stratigraphic_data____")
-# this file may include strata ages where fossils are found (could be separate file or be part of imported file)
+species.localities.stratigraphy <- read.csv("____PATH_to_file_with_ages_and_stratigraphic_data____")
+# fossil localities of all species occurences, localities repeat if multiple species are found in a location
+# this file includes strata ages where fossils are found
 # possible information that can be used in other anylses include sediment type, sedimentary environment, infered water depth, etc
 # need to think about the structure of this data base
---
+++ PaleoBioGeoFunctions.R
@@ -0,0 +1,23 @@
+library("BioGeoBEARS")
+library("pygplates")
+library("paleobioDB")
+library("rpython")
+
+coord <- function() {
+  # import locality coordinates to pygplates
+  
+}
+
+paleocoord <- function() {
+  # generate paleocoordinates in pygplates according to ages of samples
+}
+  
+  
+  ### (time is the issue here - I only need paleocoordinates for the species samples present in a basin during the times it is present. I have to consier FAD and LAD and one-offs. Also, do I marke a species present during the time of the layer it is found or the time of the full formation.)
+
+
+# map phylogeny through time?
+
+# how might I use spreading rates?
+
+# include functions "strap", "geoscale", or other such fossil and paleo packages? (including "paleoDB)
