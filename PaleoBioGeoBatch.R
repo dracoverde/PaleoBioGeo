d551204c6557d91c814d7b800f6146f2ab950c0f
Ryan Roney <Ryan Roney>
2016-05-06 13:35:54 -0400

    expanding ideas

--- PaleoBioGeoBatch.R
+++ PaleoBioGeoBatch.R
@@ -1,11 +1,21 @@
 
 source("PaleoBioGeoFunctions")
 tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
-Localities <- read.csv("___PATH_to_File_with_locality_coordinates____")
-# file with localities of of species, including GPS coordinates of locality or a centroid of the basin. localities have unique identifiers matched with those used in 'species.localities.stratigiraphy' file. These do not repeat in this file. May need to be in GMT file format
+species.locality.age <- read.csv("____PATH_TO_SPECIES_OCCURRENCE_DB____")
+# DB of each species occurence with localities, stratigraphic ages, and any other information 
+# In functions will reference the columns in the DB that refer to the needed data.
 
-species.localities.stratigraphy <- read.csv("____PATH_to_file_with_ages_and_stratigraphic_data____")
+
+Localities <- read.csv("___PATH_to_File_with_locality_coordinates____")
+# file with localities, single entry per locality, i.e. no repeats. This includes GPS coordinates of locality or a centroid of the basin. localities have unique identifiers matched with those used in 'species.localities.stratigiraphy' file. May need to be in GMT file format
+
+Stratigraphy <- read.csv("____formation/member_names_and_time_ranges____")
+
+Species.locality.strat <- read.csv("____PATH_to_file_with_location_and_stratigraphic_data_by_species____")
 # fossil localities of all species occurences, localities repeat if multiple species are found in a location
 # this file includes strata ages where fossils are found
 # possible information that can be used in other anylses include sediment type, sedimentary environment, infered water depth, etc
 # need to think about the structure of this data base
+
+GetPaleoCoordinates <- paleocoord(Localities)
+PaleoBioGeo <- combine_tree_and_paleocoordinates (tree, )
--- PaleoBioGeoFunctions.R
+++ PaleoBioGeoFunctions.R
@@ -1,25 +1,27 @@
 library("BioGeoBEARS")
-library("paleobioDB")
-library("rpython") 
+library("PythonInR") 
+# one may also use the 'rpyhton' package instead, but i had difficult setting this one up in windows 
 # will need python and pygplates installed on computer
 # pygplates is the python version of GPlates
 
-locality.coord <- function() {
+locality.coord <- function(data) {
   # import locality coordinates to pygplates
   # looks like I need to have a file in GMT format (can just use a .txt file and change suffix)
   
 }
 
-paleocoord <- function() {
+paleocoord <- function(data, timedepth=250, timeend=0) {
+  locality.coord(data)
   # generate paleocoordinates in pygplates according to ages of samples
   # looks like gplates generates coordinates for each location along 1 My intervals
   # pygplates will save in GMT format, can I save into other formats here? DO I even need to?
+  save "paleocoord.file"
 }
   
   
   ### (time is the issue here - I only need paleocoordinates for the species samples present in a basin during the times it is present. I have to consier FAD and LAD and one-offs. Also, do I marke a species present during the time of the layer it is found or the time of the full formation.)
 
-
+combine_tree_and_paleocoordinates <- function(phy, data)
 # map phylogeny through time?
 
 # how might I use spreading rates?
